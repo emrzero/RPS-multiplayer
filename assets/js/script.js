5cff0dc013f355da9b3b74e4e743102ee4cf90b5
Eliot Reyes <eliotmij@gmail.com>
2016-08-14 19:32:09 -0700

    added code for persistent storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,33 +10,162 @@
   var database = firebase.database();
 
 
-var playerOne = '';
-var playerTwo = '';
+//Begin session experiment
+// var connectionsRef = database.ref("/connections");
 
-var playerOneWeapon = '';
-var playerTwoWeapon = '';
+var connectedRef = database.ref(".info/connected");
 
-var turn = 1;
+// connectedRef.on('value', function(snapshot){
+//   if (snapshot.val()){
+//     var con = connectionsRef.push(true);
+
+//     con.onDisconnect().remove();
+//   }
+// });
+
+//End Session Experiment
+
+//Begin Read once experiment
+
+var playerNum = 0;
+var readyForMatch = false;
+
+database.ref('players').once('value').then(function(snapshot){
+  if (snapshot.val() == null){
+    console.log("no players yet");
+    playerNum = 1;
+    console.log('You are player ' + playerNum);
+    database.ref().update({chat: ''});
+  } 
+
+  else if (Object.keys(snapshot.val()).length == 1) {
+    console.log(Object.keys(snapshot.val()).length);
+    readyForMatch = true;
+    playerNum = 2;
+    console.log('You are player ' + playerNum);
+    $('#playerOneBox').find('h3').text(snapshot.val().p1.name);
+  }
+
+  else if (Object.keys(snapshot.val()).length == 2){
+    $('#sectionNameInput').html("Arena is full. No more slots!");
+    $('#playerOneBox').find('h3').text(snapshot.val().p1.name);
+    $('#playerTwoBox').find('h3').text(snapshot.val().p2.name);
+  }
+});
+
+//End Read once experiment
+
+//Begin Watch for updates Experiment
+
+var playersRef = database.ref('players');
+
+
+playersRef.on('child_added', function(snapshot, prevChildKey){
+  console.log('A new player has entered the arena!');
+  console.log(playerNum);
+  console.log(prevChildKey);
+
+  if (playerNum == 1){
+    if(readyForMatch == false) {
+      $('#playerOneBox').find('h3').text(snapshot.val().name);
+      readyForMatch = true;
+      
+    }
+    database.ref('players').once('value').then(function(snapshot){
+      try{
+        console.log(snapshot.val().p2.name);
+        $('#playerTwoBox').find('h3').text(snapshot.val().p2.name);
+        printChoices('playerOne');
+        weaponChoiceListener();
+      }
+
+      catch(err){
+        console.log("player two doesn't exist yet");
+      }
+ 
+    });
+  } else if (playerNum == 2 ){
+    $('#playerTwoBox').find('h3').text(snapshot.val().name);
+    printChoices('playerTwo');
+    weaponChoiceListener();
+    database.ref().update({turn : 1});
+    if (readyForMatch == false){
+      $('#playerOneBox').find('h3').text("Waiting for Player One");
+    }
+  }
+});
+
+//
+
+//Begin Experiment with watching removal of players
+
+playersRef.on('child_removed', function(snapshot){
+  readyForMatch == false;
+  // console.log(snapshot.val()); //Returns snapshot of removed data
+  if (playerNum == 1){
+    $('#playerTwoBox').find('h3').text("Waiting for Player Two");
+  } else if(playerNum == 2){
+    $('#playerOneBox').find('h3').text("Waiting for Player One");
+  }
+
+});
+//End Experiment with watching removal of players
+
+var playerObj = {
+  name: "",
+  wins: 0,
+  losses: 0
+}
+
+
+// var playerOne = '';
+// var playerTwo = '';
+
+// var playerOneWeapon = '';
+// var playerTwoWeapon = '';
+
+// var turn = 1;
+
 
 var choices = ['rock', 'paper', 'scissors'];
 
 $('#submitName').on('click', function(){
-  if (playerOne == ''){
-    playerOne = $('#inputName').val();
-    $('#playerOneBox').find('h3').text('Player One');
-    $('#playerOneBox').append('<h1>' + playerOne + '</h1>');
-    // printChoices('playerOne');
-    matchMessage('Wating for player two');
-  } else {
-    playerTwo = $('#inputName').val();
-    $('#playerTwoBox').find('h3').text('Player Two');
-    $('#playerTwoBox').append('<h1>' + playerTwo + '</h1>');
-    printChoices('playerOne');
-    printChoices('playerTwo');
-    matchMessage("Let's play!");
-    weaponChoiceListener();
+  playerObj.name = $('#inputName').val();
+  $('#sectionNameInput').html('<h2>Hi ' + playerObj.name + ' you are Player ' + playerNum);
+  var k = "p" + playerNum
+  var playerSession = database.ref("players").update({ [k] : playerObj});
+
+  try {
+    var ref = database.ref('players/p' + playerNum);
+    ref.onDisconnect().set(null);
   }
+
+  catch(err){
+    console.log("couldn't complete delete on onDisconnect");
+  }
+
+
 });
+
+// $('#submitName').on('click', function(){
+//   if (playerOne == ''){
+//     playerObj.name = $('#inputName').val();
+//     playerOne = $('#inputName').val();
+//     $('#playerOneBox').find('h3').text('Player One');
+//     $('#playerOneBox').append('<h1>' + playerOne + '</h1>');
+//     // printChoices('playerOne');
+//     matchMessage('Wating for player two');
+//     database.ref("/players").set({p1 : playerObj});
+//   } else {
+//     playerTwo = $('#inputName').val();
+//     $('#playerTwoBox').find('h3').text('Player Two');
+//     $('#playerTwoBox').append('<h1>' + playerTwo + '</h1>');
+//     printChoices('playerOne');
+//     printChoices('playerTwo');
+//     matchMessage("Let's play!");
+//     weaponChoiceListener();
+//   }
+// });
 
 function printChoices(player){
   for (c in choices){
@@ -54,23 +183,65 @@ function matchMessage(msg){
   $('#matchResultsTextBox').text(msg);
 }
 
-function weaponChoiceListener (){
+function weaponChoiceListener() {
   $('.weapon').on('click',function(){
-    if (turn == 1 && $(this).data('player')== 'playerOne'){
-      turn = 2;
-      playerOneWeapon = $(this).data('weapon');
-      $('#playerOneChoice').text(playerOneWeapon);
-      matchMessage('Wating for Player TWO to choose');
-
-    } else if (turn == 2 && $(this).data('player')== 'playerTwo'){
-      turn = 1;
-      playerTwoWeapon = $(this).data('weapon');
-      $('#playerTwoChoice').text(playerTwoWeapon);
-      compareWeapons(playerOneWeapon, playerTwoWeapon);
-    }
-
+    database.ref('turn').once('value').then(function(snapshot){
+      // console.log(snapshot.val());
+      // database.ref().update({turn : 1});
+      if (snapshot.val() == 1){
+        console.log("It's player one's turn, now toggling.");
+        database.ref().update({turn: 2});
+      } else{
+        console.log("It's player two's turn, now toggling.");
+        database.ref().update({turn: 1});
+      }
+    });
   });
 }
+
+// function weaponChoiceListener (){
+//   $('.weapon').on('click',function(){
+//     if (turn == 1 && $(this).data('player')== 'playerOne'){
+//       turn = 2;
+//       playerOneWeapon = $(this).data('weapon');
+//       $('#playerOneChoice').text(playerOneWeapon);
+//       matchMessage('Wating for Player TWO to choose');
+
+//     } else if (turn == 2 && $(this).data('player')== 'playerTwo'){
+//       turn = 1;
+//       playerTwoWeapon = $(this).data('weapon');
+//       $('#playerTwoChoice').text(playerTwoWeapon);
+//       compareWeapons(playerOneWeapon, playerTwoWeapon);
+//     }
+
+//   });
+// }
+
+//Begin Chat functionality
+
+$('#send').on('click', function(){
+  var msg = $('#message').val();
+  // console.log(msg);
+
+  database.ref('chat').push({name: playerObj.name, message: msg});
+  $('#message').val('');
+});
+
+var chatRef = database.ref('chat');
+
+chatRef.on('child_added', function(snapshot){
+  // console.log(snapshot.val().name);
+  // console.log(snapshot.val().message);
+
+  var l = $('<li>');
+  l.append(snapshot.val().name);
+  l.append(': ');
+  l.append(snapshot.val().message);
+  $('#chatLog').append(l);
+
+});
+
+//End Chat functionality
 
 function compareWeapons (p1, p2){
   if (p1 == p2){
