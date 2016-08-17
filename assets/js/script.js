da566879f97d068b06a28ccb10c19cf600df9da5
Eliot Reyes <eliotmij@gmail.com>
2016-08-17 00:04:55 -0700

    output: stats and messages

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,10 +35,12 @@ var playerObj = {
 var playersRef = database.ref('players');
 
 playersRef.on('child_added', function(snapshot, prevChildKey){
-  console.log(snapshot.child("name").val() + ' has entered the arena!');
+
+  database.ref('chat').push({name: "server", message: snapshot.child("name").val()+' has entered the arena!'});
 
   var outputHTML = $("#" + snapshot.key + "Box");
   outputHTML.find('h3').text(snapshot.child("name").val());
+
 
   playersRef.once('value').then(function(snap){
     if (snap.numChildren() == 1 && snap.child('p1').exists()){
@@ -50,9 +52,9 @@ playersRef.on('child_added', function(snapshot, prevChildKey){
 
   .then(function(){
     if (localMatchStats.readyForMatch == false){
-      console.log("waiting for more players")
+      database.ref('chat').push({name: "server", message: "waiting for more players"});
     } else {
-      console.log("We're ready for a match. Initializing game...");
+      database.ref('chat').push({name: "server", message: "We're ready for a match. Initializing game..."});
       initializeGame();
     }    
   }); 
@@ -64,10 +66,11 @@ playersRef.on('child_added', function(snapshot, prevChildKey){
 playersRef.on('child_removed', function(snapshot){
   localMatchStats.readyForMatch == false;
   localMatchStats.opponent = "";
-  console.log(snapshot.child('name').val() + " has left the arena"); //Returns snapshot of removed data
+  database.ref('chat').push({name: "server", message: snapshot.child("name").val()+' has left the arena!'});
+
   if (localMatchStats.me == "p1"){
     $('#p2Box').find('h3').text("Waiting for Player Two");
-  } else if(localMatchStats.me == "p1"){
+  } else if(localMatchStats.me == "p2"){
     $('#p1Box').find('h3').text("Waiting for Player One");
   } else {
     $('#p1Box').find('h3').text("Waiting for Player One");
@@ -77,7 +80,60 @@ playersRef.on('child_removed', function(snapshot){
 });
 
 
+//Listen for Score changes
+playersRef.child('p1').on('value', function(snapshot){
+    $('#p1Wins').text(snapshot.child('wins').val());
+    $('#p1Losses').text(snapshot.child('losses').val());
 
+    if(snapshot.key != localMatchStats.me){
+      localMatchStats.opponent = snapshot.child('name').val();
+    }
+  });
+
+playersRef.child('p2').on('value', function(snapshot){
+    $('#p2Wins').text(snapshot.child('wins').val());
+    $('#p2Losses').text(snapshot.child('losses').val());
+
+    if(snapshot.key != localMatchStats.me){
+      localMatchStats.opponent = snapshot.child('name').val();
+    }
+  });
+
+
+//INITIALIZE GAME
+function initializeGame(){
+  database.ref().update({chat: ""});
+  $('#chatLog').empty();
+  database.ref().update({turn: 1});
+  localMatchStats.turn = 1;
+
+  var p1ChoiceRef = database.ref('players/p1/choice');
+
+  p1ChoiceRef.on('value', function(snapshot){
+    localMatchStats.p1Choice = snapshot.val();
+    if (localMatchStats.me == "p2" && localMatchStats.p1Choice != null){
+      database.ref().update({turn: 2});
+      printChoices("p2");
+      weaponChoiceListener();
+      matchMessage('Your turn!');
+    }
+
+  });
+
+  var p2ChoiceRef = database.ref('players/p2/choice');
+  p2ChoiceRef.on('value', function(snapshot){
+    localMatchStats.p2Choice = snapshot.val();
+    if (localMatchStats.p1Choice != null 
+      && localMatchStats.p2Choice != null
+      && localMatchStats.roundOver == false){
+      compareWeapons(localMatchStats.p1Choice, localMatchStats.p2Choice);
+    }
+  });
+
+
+  beginRound();
+}
+//End INITIALIZE GAME
 
 $('#submitName').on('click', function(){
   playerObj.name = $('#inputName').val();
@@ -101,73 +157,42 @@ $('#submitName').on('click', function(){
 
 
 function printChoices(player){
+  var box = $('#' + player + 'Choice');
+  box.empty();
   for (c in choices){
     var l = $('<button>');
     l.data('weapon', choices[c]);
     l.data('player', player);
     l.addClass('weapon');
     l.html(choices[c]);
-    var box = player + 'Box';
-    $('#' + box).append(l);
+    box.append(l);
   }
+
 }
 
-function matchMessage(msg){
-  $('#matchResultsTextBox').text(msg);
-}
 
 function weaponChoiceListener() {
   $('.weapon').on('click',function(){
     localMatchStats[localMatchStats.me + "Choice"] = $(this).data('weapon');
     $('#' + localMatchStats.me + 'Choice').text($(this).data('weapon'));
     
-    console.log("You chose " + $(this).data('weapon'));
-    console.log(localMatchStats[localMatchStats.me + "Choice"]);
-    database.ref('players/' + localMatchStats.me).update({choice : $(this).data('weapon')});
+    database.ref('players/' + localMatchStats.me).update({choice : localMatchStats[localMatchStats.me + "Choice"]});
     $('.weapon').remove();
+
+    if (localMatchStats.me == "p1"){
+      matchMessage("Waiting for " + localMatchStats.opponent + " to choose.");
+    } 
   });
 }
 
-
-
-//INITIALIZE GAME
-function initializeGame(){
-  database.ref().update({chat: ""});
-  database.ref().update({turn: 1});
-  localMatchStats.turn = 1;
-
-  var p1ChoiceRef = database.ref('players/p1/choice');
-  p1ChoiceRef.on('value', function(snapshot){
-    localMatchStats.p1Choice = snapshot.val();
-    if (localMatchStats.me == "p2" && localMatchStats.p1Choice != null){
-      database.ref().update({turn: 2});
-      printChoices("p2");
-      weaponChoiceListener();
-    }
-
-  });
-
-  var p2ChoiceRef = database.ref('players/p2/choice');
-  p2ChoiceRef.on('value', function(snapshot){
-    localMatchStats.p2Choice = snapshot.val();
-    if (localMatchStats.p1Choice != null 
-      && localMatchStats.p2Choice != null
-      && localMatchStats.roundOver == false){
-      compareWeapons(localMatchStats.p1Choice, localMatchStats.p2Choice);
-    }
-  });
-
-
-  beginRound();
-}
-//End INITIALIZE GAME
 
 function beginRound(){
   if(localMatchStats.me == "p1"){
     printChoices("p1");
     weaponChoiceListener();
+    matchMessage("Your turn!");
   } else {
-    matchMessage("Wating for Opponent to choose");
+    matchMessage("Wating for " + localMatchStats.opponent + " to choose");
   }
 }
 
@@ -176,10 +201,7 @@ function nextRound(){
   database.ref('players/p1').update({choice : null});
   database.ref('players/p2').update({choice : null});
   localMatchStats.roundOver = false;
-  $('#p1Box').text('');
-  $('#p2Box').text('');
-
-
+  $('#p2Choice').text('');
   beginRound();
 }
 
@@ -187,15 +209,26 @@ function nextRound(){
 
 function compareWeapons (p1, p2){
   localMatchStats.roundOver = true;
-  console.log(p1 + " vs " + p2);
+
   if (p1 == p2){
-    console.log("Tie!");
+    matchMessage("Tie!");
   }
+  //P1 WINS if
   else if((p1 == 'rock' && p2 == 'scissors') ||
           (p1 == 'paper' && p2 == 'rock') ||
           (p1 == 'scissors' && p2 == 'paper')
     ) {
-    console.log("Player ONE wins!");
+
+    switch(localMatchStats.me){
+      case("p1"):
+        matchMessage("You win!");
+        break;
+
+      case("p2"):
+        matchMessage("You loose.");
+        break;
+    }
+
     playersRef.child('p1/wins').transaction(function(v){
       return v + 0.5;
     });
@@ -205,11 +238,23 @@ function compareWeapons (p1, p2){
     });
   }
 
+  //P2 Wins if
   else if ((p1 == 'rock' && p2 == 'paper') ||
             (p1 == 'paper' && p2 == 'scissors') ||
             (p1 == 'scissors' && p2 == 'rock')
     ) {
-    console.log("Player TWO wins!");
+    // console.log("Player TWO wins!");
+
+    switch(localMatchStats.me){
+      case("p2"):
+        matchMessage("You win!");
+        break;
+
+      case("p1"):
+        matchMessage("You loose.");
+        break;
+    }
+
     playersRef.child('p2/wins').transaction(function(v){
       return v+ 0.5;
     });
@@ -220,9 +265,15 @@ function compareWeapons (p1, p2){
 
   }
 
-  setTimeout(nextRound, 2000);
+  setTimeout(nextRound, 3000);
 }
 
+//End CompareWeapons()
+
+
+function matchMessage(msg){
+  $('#matchResultsTextBox').text(msg);
+}
 
 
 
@@ -239,8 +290,6 @@ $('#send').on('click', function(){
 var chatRef = database.ref('chat');
 
 chatRef.on('child_added', function(snapshot){
-  // console.log(snapshot.val().name);
-  // console.log(snapshot.val().message);
 
   var l = $('<li>');
   l.append(snapshot.val().name);
